4ee4dbcc83321d220384ab07c7e5eba23d812d61
dcvcordeiro <cordeiro.dcv@gmail.com>
2024-05-29 16:51:26 +0100

    Add method for user input

--- wordle.py
+++ wordle.py
@@ -10,8 +10,41 @@ def launch_game():
     """
     Main controller for the game flow
     """
-    print("Hello World!")
+    user_guesses = []
+    solution = get_solution()
 
+    for _ in range(0,6):
+        user_guess = ask_user_guess()
+        user_guesses.append(user_guess)
+        if check_word(user_guess, solution):
+            print("Correct Solution")
+            return
+
+
+def get_solution() -> str:
+    """Generate a pseudo-random solution for the game"""
+    return "tests"
+
+
+def ask_user_guess() -> str:
+    user_guess = ""
+    while not check_guess_validity(user_guess):
+        user_guess = input("Enter guess: ")
+        user_guess = user_guess.lower()
+    return user_guess
+
+
+def check_guess_validity(user_guess:str) -> bool:
+    if user_guess == "":
+        return False
+
+    if len(user_guess) != 5 or not user_guess.isalpha():
+        print("The guess must have 5 letters")
+        return False
+    
+    return True
+    
+    # hook to check if it is a valid word
 
 def check_word(guess:str, answer:str) -> bool:
     """Checks if the user guess is the correct answer"""
@@ -20,6 +53,7 @@ def check_word(guess:str, answer:str) -> bool:
 
 def check_attempt(guess:str, answer:str) -> list(LETTER):
     """
+        Compare user guess with solution and return a color coded mapping
     """
     validation = []
     for i, j in zip(guess, answer):
@@ -33,7 +67,4 @@ def check_attempt(guess:str, answer:str) -> list(LETTER):
 
 
 if __name__ == "__main__":
-
     launch_game()
-    validation = check_attempt("test","part")
-    print(validation)
