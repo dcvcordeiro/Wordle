9d0f6590b7fe742b20fe5fcf2ab3cc5ecc730621
dcvcordeiro <cordeiro.dcv@gmail.com>
2024-06-21 15:03:13 +0100

    Update wordle.py
    - Add console enrichment
    - Add pseudo-random solution generation
    - Add valid gueses

--- wordle.py
+++ wordle.py
@@ -1,36 +1,60 @@
-from enum import Enum
+from random import choice
+from rich.console import Console
+from rich.theme import Theme
 
-class LETTER(Enum):
-    WRONG = '#D3D3D3'     # Letter does not exist in the answer (Light Gray)
-    PARTIAL = '#FFFF33'   # Letter exists in the answer, but wrong place (Yellow)
-    CORRECT = '#00FF00'   # Letter exists and it is right place (Green)
+from string import ascii_uppercase
 
+
+NR_GUESSES = 6
+NR_LETTERS = 5
+
+console = Console(width=50, theme=Theme({"warning": "red on yellow"}))
+
+def refresh_page(headline):
+    console.clear()
+    console.rule(f"[bold blue] :jigsaw:  {headline} :jigsaw:[/]\n")
 
 def launch_game():
     """
     Main controller for the game flow
     """
-    user_guesses = []
+    user_guesses = ["_" * NR_LETTERS] * NR_GUESSES
     solution = get_solution()
 
-    for _ in range(0,6):
-        user_guess = ask_user_guess()
-        user_guesses.append(user_guess)
-        if check_word(user_guess, solution):
-            print("Correct Solution")
-            return
+    for i in range(NR_GUESSES):
+        refresh_page(f"GUESS {i+1}")
 
+        show_guesses(user_guesses, solution)
+        user_guesses[i] = ask_user_guess(user_guesses)
+
+        if user_guesses[i] == solution:
+            break
+
+    game_over(user_guesses, solution, user_guesses[i] == solution)
+
+def game_over(user_guesses, solution, answered_correctly):
+    refresh_page(f"GAME OVER")
+    show_guesses(user_guesses, solution)
+
+    if answered_correctly:
+        console.print(f"\n[bold white on green]Correct, the word is {solution}[/]")
+    else:
+        console.print(f"\n[bold white on red]Sorry, the word was {solution}[/]")
 
 def get_solution() -> str:
     """Generate a pseudo-random solution for the game"""
-    return "tests"
+    with open("word_lists/wordle_answers.txt", "r") as fp:
+        valid_answers = fp.read().split("\n")
+    return choice(valid_answers).upper()
 
 
-def ask_user_guess() -> str:
+def ask_user_guess(previous_guesses) -> str:
     user_guess = ""
-    while not check_guess_validity(user_guess):
-        user_guess = input("Enter guess: ")
-        user_guess = user_guess.lower()
+    while not check_guess_validity(user_guess) and user_guess not in previous_guesses:
+        user_guess = console.input("Enter guess: ")
+        user_guess = user_guess.upper()
+        if user_guess in previous_guesses:
+            console.print(f"You have already guessed: {user_guess}")
     return user_guess
 
 
@@ -39,32 +63,38 @@ def check_guess_validity(user_guess:str) -> bool:
         return False
 
     if len(user_guess) != 5 or not user_guess.isalpha():
-        print("The guess must have 5 letters")
+        console.print("The guess must have 5 letters", style="warning")
+        return False
+    
+    with open("word_lists/valid_wordle_words.txt", "r") as fp:
+        valid_guesses = fp.read().split("\n")
+    
+    if user_guess.lower() not in valid_guesses:
+        console.print("Guess is not a valid word", style="warning")
         return False
     
     return True
-    
-    # hook to check if it is a valid word
 
-def check_word(guess:str, answer:str) -> bool:
-    """Checks if the user guess is the correct answer"""
-    return guess == answer
+def show_guesses(user_guesses, solution):
+    letter_status = {letter: letter for letter in ascii_uppercase}
 
+    for guess in user_guesses:
+        styled_guess = []
+        for guess_letter, correct_letter in zip(guess, solution):
+            if guess_letter == correct_letter:
+                style = "bold white on green"
+            elif guess_letter in solution:
+                style = "bold white on yellow"
+            elif guess_letter != "_":
+                style = "white on #666666"
+            else:
+                style = "dim"
+            styled_guess.append(f"[{style}]{guess_letter}[/]")
 
-def check_attempt(guess:str, answer:str) -> list(LETTER):
-    """
-        Compare user guess with solution and return a color coded mapping
-    """
-    validation = []
-    for i, j in zip(guess, answer):
-        if i == j:
-            validation.append(LETTER.CORRECT)
-        elif i in answer:
-            validation.append(LETTER.PARTIAL)
-        else:
-            validation.append(LETTER.WRONG)
-    return validation
+            if guess_letter != "_":
+                letter_status[guess_letter] = f"[{style}]{guess_letter}[/]"
 
-
+        console.print("".join(styled_guess), justify="center")
+    console.print("\n" + "".join(letter_status.values()), justify="center")
 if __name__ == "__main__":
     launch_game()
